1a878f77c8457b60306a04ca48c187b43fcd3932
Koopa <codingkoopa@gmail.com>
2022-11-03 00:57:28 -0400

    course_parser: improve samples

--- course_parser.py
+++ course_parser.py
@@ -6,18 +6,16 @@ from typing import Tuple
 from course import Course
 from bing_subjects import subjects, prog_course_and_title, prog_course
 
+# Sample titles:
+# CS 140 - Prog with Obj & Data Struct
+# CS 185A - Living/Learning Computer Proj
+
 # Sample descriptions:
-
 # Prerequisite: none.  Offered in the Fall semester.
-
 # Prerequisite: none Offered every semester. 4 credits
-
 # Prerequisite: Math 225 (May be taken concurrently. )  (All prerequisites must have a grade of C- or better).Offered every semester. 4 credits
-
 # Prerequisites: HARP 170 and enrollment in Freshman Research Immersion (FRI) program (All prerequisites must have a grade of C- or better).  Offered in the Spring semester.
-
 # Prerequisite MATH 225 that may be taken concurrently. CS 110, CS Majors may request a waiver from the Undergraduate Director based on prior programming experience (All prerequisites must have a grade of C- or better)
-
 # Prerequisite: CS 110,CS Majors may request a waiver from the Undergraduate Director based on prior programming experience.  Math 225 (All prerequisites must have a grade of C- or better). Offered every semester.
 
 
